c582f8788aad32565ee71641ee216f6af4928dcf
SebastienC <sebastien.cifuentes@gmail.com>
2023-12-20 15:50:34 +0100

    maj css des banners

--- src/components/Bannerabout/Bannerabout.jsx
+++ src/components/Bannerabout/Bannerabout.jsx
@@ -6,9 +6,6 @@ export default function Bannerabout() {
             <div className="banner-img">
                 <img src={bannerabout} alt="bannerabout" />
             </div>
-            <div className="banner-text-container">
-                <div className="banner-text">Chez vous, partout et ailleurs</div>
-            </div>
         </div>
     )
 }

d1f3acf76cbcc182c58bf276e78b1da7301e46f5
SebastienC <sebastien.cifuentes@gmail.com>
2023-12-22 11:54:07 +0100

    Modif banner home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import Bannerhome from '../Bannerhome/Bannerhome'
+import React from 'react';
+import Bannerhome from '../Bannerhome/Bannerhome';
+import data from '../../datas/logements.json';
 
-export default function Home() {
-    return (
-        <div>
-            <Bannerhome />
-            <h1>Emplacement des cards</h1>
-        </div>
-    )
-}
+export const Home = () => {
+  console.log(data);
+  return (
+    <div>
+      <Bannerhome />
+      <h1>Emplacement des cards</h1>
+      {data.map((d) => (
+        <p>{d.title}</p>
+      ))}
+    </div>
+  );
+};

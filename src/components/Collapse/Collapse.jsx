9c9d6913204f4664eb6891c8ddfee5770754de8a
SebastienC <sebastien.cifuentes@gmail.com>
2024-01-11 17:08:14 +0100

    Modif collapse page about

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,8 +1,29 @@
+import React, { useRef, useState } from 'react';
+import chevron from '../../assets/chevron.png';
+
 export default function Collapse({title, texte}) {
-    return (
-        <div>
-            <h1>{title}</h1>
-            <p>{texte}</p>
+    
+    const [open, setOpen] = useState(false);
+
+    const toggle = () => {
+        setOpen(!open);
+    }
+
+    const collapseTexteRef = useRef();
+
+    return(
+        <div className='collapseContainer'>
+            <div className='collapseTitleContainer primaryBackground'>
+                <h1 className='collapseTitle'>{title}</h1>
+                <p className='collapseButton' onClick={toggle}>
+                    <img src={chevron} className={(!open ? "down" : "up") + " chevron"} alt='chevron' />
+                </p>
+            </div>
+            <div className="collapseTexteContainer secondaryBackground" ref={collapseTexteRef} style={open ? {height: collapseTexteRef.current.scrollHeight + "px"} : {height: "0px"}}>
+                <div className='primary collapseTexte'>{texte}</div>
+            </div>
+
         </div>
     )
 }
+
--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import BannerImage from '../../assets/bannerabout.png';
-import Collapse from "../Collapse/Collapse"
-import dataAbout from "../../datas/about.json"
+import Collapse from '../Collapse/Collapse';
+import dataAbout from '../../datas/about.json';
 
 export default function About() {
+  document.title = 'Kasa | A Propos';
+
   return (
-    <div>
+    <div className='mainAbout'>
       <Banner pathimage={BannerImage} text={''} />
-      <h1>A PROPOS</h1>
-      {dataAbout.map((d) => (
+      <div className="collaspse_container">
+        {dataAbout.map((d) => (
           <Collapse title={d.title} texte={d.texte} />
         ))}
+      </div>
     </div>
   );
 }

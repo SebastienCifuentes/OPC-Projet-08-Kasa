9c2a8a9cab7cc31fc756246be0a6ed723c21250a
SebastienC <sebastien.cifuentes@gmail.com>
2024-01-11 19:34:52 +0100

    CSS cards

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -11,7 +11,8 @@ export const Card = (props) => {
 
   return (
     <Link to={url} className="card_logement"> {/* Ici css d'une card */}
-      <img src={props.cover} alt="Cover img" />
+      <div className="cardBg"></div>
+      <img className="cardCover" src={props.cover} alt="Cover img" />
       <div className="card_logement_title">{props.title}</div>
     </Link>
   );

527c679ce94d969f28d86e504e478ebe19c10997
SebastienC <sebastien.cifuentes@gmail.com>
2024-01-16 11:52:59 +0100

    css de la page Notfound

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom';
 
 export default function NotFound() {
   return (
-    <div>
-      <p>404</p>
-      <p>Oups! La page que vous demandez n'existe pas.</p>
-      <Link to="/">Retourner sur la page d'accueil</Link>
+    <div className="notfound_container">
+      <p className="notfound_number">404</p>
+      <p className="notfound_text">Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className="notfound_backbtn">Retourner sur la page d'accueil</Link>
     </div>
   );
 }
